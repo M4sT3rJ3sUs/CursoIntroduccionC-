674e05db0c65f043ef2c852fc63a46d83d09e40d
Jesús Alejandro Pérez Ramírez <99446191+M4sT3rJ3sUs@users.noreply.github.com>
2022-05-03 11:05:54 -0500

    3

---
+++ SquareArea/SquareArea/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //rectangle area calc.
+
+            //rectangle values
+            Console.Write("Please enter the side A of the rectangle, you can use decimals: ");
+            double sideA = double.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the side B of the rectangle, you can use decimals: ");
+            double sideB = double.Parse(Console.ReadLine());
+
+            //rectangle area formula is a*b
+            double area = sideA * sideB;
+
+            Console.WriteLine("The rectangle area is: " + area);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StringExample/StringExample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name? Please write your full name!");
+            var fullUserName = Console.ReadLine();
+            Console.WriteLine("Hello " + fullUserName + ", Welcome to Platzi");
+
+            Console.ReadKey();
+        }
+    }
+}
